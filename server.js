b6f77d8d72d6014b340fa0a3423517c506410048
Oumar Diane <oumardiane399@gmail.com>
2025-09-14 15:42:52 +0200

    nrkn4

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174",
+      "http://localhost:5174"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
